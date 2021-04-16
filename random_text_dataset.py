784fef3cc782a49409632767e734b2be875b1706
Jouni Sunnari <dz4tune@gmail.com>
2021-04-17 02:09:58 +0300

    added mindepth

--- random_text_dataset.py
+++ random_text_dataset.py
@@ -23,7 +23,7 @@ class RandomTextDataset(Dataset):
         line_height = int(letter_height * 1.2)
 
 
-        tree = random_page_tree(maxdepth=4)
+        tree = random_page_tree(mindepth=1, maxdepth=3)
         img = Image.new('L', (1800, 1000), 'white')
         draw = ImageDraw.Draw(img)
         tree.draw_on(draw, (0, 0, img.width, img.height), debug=True)
@@ -32,13 +32,17 @@ class RandomTextDataset(Dataset):
 def random_string(length: int):
     return ''.join((random.choice(string.ascii_letters + ' ' * 10) for _ in range(length)))
 
-def random_page_tree(maxdepth: int):
-    node = random.randint(0, 2)
+def random_page_tree(mindepth: int, maxdepth: int):
+    if mindepth == 0:
+        node = random.randint(0, 2)
+    else:
+        node = random.randint(1, 2)
+
     if node == 0 or maxdepth == 0:
         font = ImageFont.truetype(random.choice(os.listdir('/usr/share/fonts/TTF')), random.randint(8, 16))
         text = '    ' + random_string(3000)
         return page_tree.Pad(0.05, 0.05, 0.05, 0.05, page_tree.TextLeaf(font=font, text=text))
     elif node == 1:
-        return page_tree.YSplit(0.2 + 0.6 * random.random(), random_page_tree(maxdepth-1), random_page_tree(maxdepth-1))
+        return page_tree.YSplit(0.2 + 0.6 * random.random(), random_page_tree(mindepth-1, maxdepth-1), random_page_tree(mindepth-1, maxdepth-1))
     elif node == 2:
-        return page_tree.XSplit(0.2 + 0.6 * random.random(), random_page_tree(maxdepth-1), random_page_tree(maxdepth-1))
+        return page_tree.XSplit(0.2 + 0.6 * random.random(), random_page_tree(mindepth-1, maxdepth-1), random_page_tree(mindepth-1, maxdepth-1))
